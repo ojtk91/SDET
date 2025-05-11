7dadde24f9ed7ad35abf7313273d03ecbcb59277
oj tk91 <ojtk91@gmail.com>
2025-05-12 00:50:22 +0530

    new push

---
+++ src/test/java/Test/Practice_02.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Practice_02 {
+    public static void main(String[] args) {
+        System.out.println("Practice_03");
+    }
+    public static void main(String args) {
+        System.out.println("Practice_03");
+    }
+    public static void main(int args) {
+        System.out.println("Practice_03");
+    }
+
+}
